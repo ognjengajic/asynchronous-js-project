b90ac7a0e2b6bc2abf01d917d856b99435f095bc
ognjengajic <ognjeng535@gmail.com>
2026-05-15 19:10:41 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+// NEW REVERSE GEOCODING API URL:
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+
+///////////////////////////////////////
