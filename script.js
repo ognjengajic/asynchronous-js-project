17708599232efe247a10b17c6047eed49e0f63fe
ognjengajic <ognjeng535@gmail.com>
2026-05-21 12:48:43 +0200

    Other Promise Combinators: race, allSettled and any

--- script.js
+++ script.js
@@ -472,3 +472,53 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries(`portugal`, `serbia`, `tanzania`);
+
+//Promise.race
+
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/serbia`),
+    getJSON(`https://restcountries.com/v3.1/name/germany`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long!`));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v3.1/name/tanzania`),
+  timeout(5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.log(err));
+
+//Promise.allSettled
+Promise.allSettled([
+  Promise.resolve(`Success`),
+  Promise.reject(`ERROR`),
+  Promise.resolve(`Another success`),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve(`Success`),
+  Promise.reject(`ERROR`),
+  Promise.resolve(`Another success`),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+//Promise.any
+Promise.any([
+  Promise.resolve(`Success👍👍👍`),
+  Promise.reject(`ERROR`),
+  Promise.resolve(`Another success`),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
